fb3fc530a7526796c54d415236728ba321224de1
Felipe Abreu <felipeabreu.rj@gmail.com>
2026-01-20 17:55:07 -0300

    1.0.5

--- src/django_env_loader/__init__.py
+++ src/django_env_loader/__init__.py
@@ -1,5 +1,19 @@
-from .exceptions import SecretNotFoundError, ValidationError
-from .loader import DjangoEnvLoader, EnvConfig, EnvLoader
+"""Django Env Loader - Gerenciamento de variáveis de ambiente."""
 
-__version__ = "1.0.4"
-__all__ = ["EnvLoader", "DjangoEnvLoader", "EnvConfig", "SecretNotFoundError", "ValidationError"]
+from django_env_loader.exceptions import EnvLoaderError, SecretNotFoundError, ValidationError
+from django_env_loader.loader import DjangoEnvLoader, EnvConfig, EnvLoader
+
+__version__ = "1.0.5"
+__all__ = [
+    "EnvLoader",
+    "DjangoEnvLoader",
+    "EnvConfig",
+    "EnvLoaderError",
+    "SecretNotFoundError",
+    "ValidationError",
+    "env_loader",
+]
+
+# Instância singleton padrão para importação direta
+# Uso: from django_env_loader import env_loader
+env_loader = EnvLoader()
